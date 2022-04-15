6b039d3ada814db144f665a62cc209d4ed535e57
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-16 02:56:03 +0300

    r

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] CharacterController2D characterController;
-    [SerializeField] float walkVelocity;
-
-    [SerializeField] Animator animator;
-
-    float horizontal;
-    float vertical;
-    bool jump;
-    private void Update()
-    {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        jump = Input.GetKeyDown(KeyCode.Space);
-    }
-    private void FixedUpdate()
-    {
-        characterController.Move(horizontal*walkVelocity, false, jump);
-    }
+    
 }
