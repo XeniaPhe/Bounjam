cb4ee11bef863566307df2bd1c24262c621e4abd
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-16 02:18:10 +0300

    abc

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,72 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] new Rigidbody2D rigidbody;
+    [SerializeField] CharacterController2D characterController;
     [SerializeField] float walkVelocity;
-    [SerializeField] float climbVelocity;
-    [SerializeField] float jumpVelocity;
+
     [SerializeField] Animator animator;
-    bool isJumping;
-    bool isWalking;
-    bool isClimbing;
-    bool isGrounded;
+
+    float horizontal;
+    float vertical;
+    bool jump;
     private void Update()
     {
-        CheckGrounded();
-        TakeInputs();
-        UpdateAnimations();
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        jump = Input.GetKeyDown(KeyCode.Space);
     }
-
-    public void CheckGrounded()
+    private void FixedUpdate()
     {
-        isGrounded = true;
-    }
-
-    public void TakeInputs()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isJumping = true;
-            rigidbody.velocity.Set(0, jumpVelocity);
-        }
-        else
-        {
-            isJumping = false;
-            rigidbody.velocity.Set(0, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //climb ladder up
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            //climb ladder down
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            //go left
-            transform.localScale.Set(transform.localScale.x, -1, transform.localScale.z);
-            rigidbody.velocity = Vector2.left*walkVelocity;
-            if (isGrounded)
-                isWalking = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            //go right;
-            transform.localScale.Set(transform.localScale.x,1, transform.localScale.z);
-            rigidbody.velocity = Vector2.right*walkVelocity;
-            if (isGrounded)
-                isWalking = true;
-        }
-    }
-
-    public void UpdateAnimations()
-    {
-        animator.SetBool("Jumping", isJumping);
-        animator.SetBool("Walking", isWalking);
-        animator.SetBool("Climbing", isClimbing);
+        characterController.Move(horizontal*walkVelocity, false, jump);
     }
 }
