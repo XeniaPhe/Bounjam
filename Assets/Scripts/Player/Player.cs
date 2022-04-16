d9f1bd1b8e3c1d9d58b20d07614363f350664a5d
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-16 05:14:50 +0300

    minor changes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -95,6 +95,8 @@ public class Player : MonoBehaviour
     }
     private void UpdateAnimations()
     {
+        if (!animator) return;
+
         if (isJumping && !isJumpingAnimated)
         {
             animator.SetBool("Jumping", true);
