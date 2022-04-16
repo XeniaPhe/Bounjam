3d07a2a6ae4615bcad9a7ea7b02ec1fb43ba0e31
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-16 18:59:53 +0300

    abc

---
+++ Assets/Scripts/Inventory/ItemDetailsMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class ItemDetailsMenu : MonoBehaviour
+{
+    [SerializeField] TMP_Text itemNameText;
+    [SerializeField] TMP_Text descriptionText;
+    [SerializeField] TMP_Text statusText;
+    [SerializeField] Image itemIcon;
+}
