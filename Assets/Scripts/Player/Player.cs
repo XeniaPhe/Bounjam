a69bc4359f1d8a81c4e6ab4005de91587ff9d163
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-16 04:51:55 +0300

    a

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    new Rigidbody2D rigidbody;
+    Animator animator;
+
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] bool airControl = true;
+    [SerializeField] Transform head;
+    [SerializeField] float checkRadius = 0.05f;
+    [SerializeField] Transform feet;
+
+    float horizontal, vertical;
+
+    [SerializeField] LayerMask groundLayer;
+
+    bool isGrounded, isBlocked,isJumping, isRunning,_jump;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        CheckPosition();
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && isGrounded && !isBlocked && !_jump)
+            _jump = true;
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void CheckPosition()
+    {
+        if (Physics2D.OverlapCircle(head.position, checkRadius, groundLayer.value))
+        {
+            isBlocked = true;
+            isJumping = false;
+        }
+        else
+            isBlocked = false;
+        if (Physics2D.OverlapCircle(feet.position, checkRadius, groundLayer.value))
+        {
+            isGrounded = true;
+            isJumping = false;
+        }
+        else
+            isGrounded = false;
+    }
+
+    public void Move()
+    {
+        if(_jump)
+        {
+            _jump = false;
+            isJumping = true;
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        }
+
+        isRunning = true;
+        if (horizontal > 0)
+        {
+            rigidbody.velocity = new Vector2(runSpeed, rigidbody.velocity.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontal < 0)
+        {
+            rigidbody.velocity = new Vector2(-runSpeed, rigidbody.velocity.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontal == 0 && rigidbody.velocity.x == 0)
+            isRunning = false;
+    }
 }
