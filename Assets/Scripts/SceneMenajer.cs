3505375bcd200ee7d0ee19734285702025ed0d3e
XeniaPhe <m.tunahanbas90@gmail.com>
2022-04-17 14:49:45 +0300

    a

---
+++ Assets/Scripts/SceneMenajer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneMenajer : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
