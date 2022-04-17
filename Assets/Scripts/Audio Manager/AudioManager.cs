31afcf1615ec21c90a69abe67723154a74f9e7af
cgok <44663880+cgok@users.noreply.github.com>
2022-04-17 19:42:07 +0300

    Update

---
+++ Assets/Scripts/Audio Manager/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip ambianceAudioClip;
+    public AudioClip battleAudioClip;
+
+    public void PlayBattleMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = battleAudioClip;
+        audioSource.Play();
+    }
+
+    public void PlayAmbianceMusic()
+    {
+        audioSource.Stop();
+        audioSource.clip = ambianceAudioClip;
+        audioSource.Play();
+    }
+}
