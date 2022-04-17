aee61fbd502ad4561f995284d8ca3508a4148b7f
cgok <44663880+cgok@users.noreply.github.com>
2022-04-17 18:15:18 +0300

    Update

---
+++ Assets/Scripts/Player/PlayerAutoSave.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAutoSave : MonoBehaviour
+{
+    public Statue lastStatue;
+
+    public void Continue()
+    {
+        if (!lastStatue) return;
+        else
+        {
+            Player.Instance.transform.position = lastStatue.transform.position;
+            PlayerStats.Instance.health = PlayerStats.Instance.maxHealth;
+        }
+    }
+}
